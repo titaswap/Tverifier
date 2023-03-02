ee0e83a69bb8b0589863e54aace9290a0093ab69
titaswap <titaswap0@gmail.com>
2023-03-02 06:14:24 +0600

    some update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const api = async (id) => {
+const api = async (id, idEmail) => {
     const result = document.getElementById(id)
     result.innerText = 'Loding...'
-    const email = document.getElementById('email').value;
+    const email = document.getElementById(idEmail).value;
     const url = `https://api.my-addr.com/email/api.php?secret=F62AA8928ECDB5622E9E038B178E0802&email=${email}`
     fetch(url)
         .then(res => res.text())
@@ -9,7 +9,17 @@ const api = async (id) => {
 }
 
 const resultAdd = (response, id) => {
-  
     const result = document.getElementById(id)
     result.innerText = `${response}`
 }
+
+document.querySelector('#email-1').addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        api('result-1', 'email-1')
+    }
+});
+document.querySelector('#email-2').addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        api('result-2', 'email-2')
+    }
+});
